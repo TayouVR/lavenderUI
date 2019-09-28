b294bbaf55bb12d0de64adc1328f3999883aba75
tayou <tayou@gmx.net>
2019-09-28 11:38:37 +0200

    removed unneccessary JS logging. slightly changed dark color scheme.

--- main.js
+++ main.js
@@ -10,11 +10,6 @@ function previousPage() {
 	game.PreviousPage();
 }
 
-function updateSearchSettings(personal) {
-	document.getElementById("personalText").innerText = "Personal: " + personal;
-}
-
-
 function updateAssetInfo(asset) {
 
 	var a = JSON.parse(asset);
@@ -375,21 +370,16 @@ function SettingsPage(newState) {
 	//3 Online
 	//4 UI
 
-	//var newState = newState.parseInt();
-	console.log("[JS] newState type = " + typeof newState);
-
 	console.log("[JS] Switching settings page to " + newState);
 
 	//Grab the various elements we care about
 
-	console.log("[JS] are pages the error?");
 	var gameplay = document.getElementById("gameplaySettings");
 	var graphics = document.getElementById("graphicsSettings");
 	var audio = document.getElementById("audioSettings");
 	var online = document.getElementById("onlineSettings");
 	var ui = document.getElementById("uiSettings");
 
-	console.log("[JS] are buttons the error?");
 	//buttons
 	var gameplayButton = document.getElementById("gameplayButton");
 	var graphicsButton = document.getElementById("graphicsButton");
@@ -397,14 +387,12 @@ function SettingsPage(newState) {
 	var onlineButton = document.getElementById("onlineButton");
 	var uiButton = document.getElementById("uiButton");
 
-	console.log("[JS] is button active removing the error?");
 	gameplayButton.classList.remove("active");
 	graphicsButton.classList.remove("active");
 	audioButton.classList.remove("active");
 	onlineButton.classList.remove("active");
 	uiButton.classList.remove("active");
 
-	console.log("[JS] is page hiding the error?");
 	//Hide all of them
 	gameplay.style.display = "none";
 	graphics.style.display = "none";
@@ -412,7 +400,6 @@ function SettingsPage(newState) {
 	online.style.display = "none";
 	ui.style.display = "none";
 
-	console.log("[JS] is switch the error?");
 	//Show the ones for the state we just entered
 	switch (newState) {
 		case 0:
