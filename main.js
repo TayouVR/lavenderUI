c1279aed031a545d72aa1a1b4d1645e6b05603b8
Tayou <tayou@gmx.net>
2019-12-08 17:54:52 +0100

    fixed clock

--- main.js
+++ main.js
@@ -13,28 +13,20 @@ var icons = [iconsDark, iconsLight];
 function updateClock () {
   var currentTime = new Date ( );
 
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
+  var currentHours = currentTime.getHours();
+  var currentMinutes = currentTime.getMinutes();
+  var currentSeconds = currentTime.getSeconds() + 1;
 
   // Pad the minutes and seconds with leading zeros, if required
+  currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
   currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-  // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
   // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
   // Update the time display
-  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+  document.getElementsByClassName("clock")[0].firstChild.nodeValue = currentTimeString;
 }
 
 /*window.onload = function() {
